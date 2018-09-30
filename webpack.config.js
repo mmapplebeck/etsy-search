f00284861dfde0d989bff161b002ef1fc1056042
Matthew Mapplebeck <matthewmapplebeck@ABT-MM-H04168.local>
2018-09-29 22:27:04 -0400

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   devServer: {
@@ -53,4 +53,4 @@ module.exports = {
       chunkFilename: '[id].css'
     })
   ]
-};
+}

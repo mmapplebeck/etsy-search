62eb028cf07d0384079843e374fc7692b79f2408
Matthew Mapplebeck <matthewmapplebeck@ABT-MM-H04168.local>
2018-09-30 00:42:55 -0400

    support pagination

--- src/index.js
+++ src/index.js
@@ -4,34 +4,55 @@ import getCard from './components/Card'
 const formEl = document.querySelector('.search')
 const queryEl = formEl.querySelector('.search__query')
 const resultsEl = document.querySelector('.results')
+const nextEl = document.querySelector('.next')
 const apiKey = 'rrok6oy5kysthf2v2fyh4qja'
+let page = 0
+let query
+
+function encodedQuery() {
+  return encodeURIComponent(query)
+}
 
 formEl.addEventListener('submit', e => {
   e.preventDefault()
 
-  const query = queryEl.value
+  query = queryEl.value
   if (!query) return
 
   routie(`listings/${query}`)
-  fetchResults(query)
+  getResults()
 });
 
-function fetchResults(query) {
-  const encodedQuery = encodeURIComponent(query)
+nextEl.addEventListener('click', fetchResults)
 
-  if (localStorage.getItem(encodedQuery)) {
-    displayResults(JSON.parse(localStorage.getItem(encodedQuery)));
+function getResults() {
+  resultsEl.innerHTML = ''
+
+  const q = encodedQuery()
+
+  if (localStorage.getItem(q)) {
+    const cached = JSON.parse(localStorage.getItem(q))
+
+    page = cached.page
+    displayResults(cached.results);
     return
   }
 
+  fetchResults()
+}
+
+function fetchResults() {
   const script = document.createElement('script')
 
-  script.setAttribute('src', `https://openapi.etsy.com/v2/listings/active.js?callback=EtsySearch.onFetch&api_key=${apiKey}&limit=5&keywords=${query}&fields=title,url,price`)
+  script.setAttribute('src', `https://openapi.etsy.com/v2/listings/active.js?callback=EtsySearch.onFetch&api_key=${apiKey}&limit=1&page=${++page}&keywords=${query}&fields=title,url,price`)
   document.head.appendChild(script)
   document.head.removeChild(script)
 }
 
-routie('listings/:query', fetchResults);
+routie('listings/:query', q => {
+  query = q
+  getResults()
+});
 
 function displayResults(results) {
   results.forEach(result => {
@@ -40,9 +61,22 @@ function displayResults(results) {
 }
 
 export function onFetch(data) {
-  displayResults(data.results)
+  query = data.params.keywords
+  
+  const q = encodedQuery()
+  let results = data.results
+
+  displayResults(results)
+
+  if (localStorage.getItem(q)) {
+    results = JSON.parse(localStorage.getItem(q)).results.concat(results)
+  }
+
   try {
-    localStorage.setItem(encodeURIComponent(data.params.keywords), JSON.stringify(data.results))
+    localStorage.setItem(q, JSON.stringify({
+      page: data.params.page,
+      results
+    }))
   } catch(err) {
   }
 }

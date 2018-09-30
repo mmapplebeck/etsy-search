84a263aa3d03da1fac516d4d860951fa2e068c6c
Matthew Mapplebeck <matthewmapplebeck@ABT-MM-H04168.local>
2018-09-30 02:05:02 -0400

    fetch images and add some card styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,11 @@
 import template from './index.ejs'
+import style from './style.scss'
 
 export default function(data) {
-  const card = document.createElement('div')
+  const card = document.createElement('a')
   card.classList.add('card')
+  card.setAttribute('href', data.url)
+  card.setAttribute('data-listing-id', data.listing_id)
   card.innerHTML = template(data)
   return card
 }

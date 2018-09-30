60566d3ec0a7230a7c31b6b8a9ff0075694f8ca5
Matthew Mapplebeck <matthewmapplebeck@ABT-MM-H04168.local>
2018-09-29 23:35:07 -0400

    support bookmarkable queries

--- src/index.js
+++ src/index.js
@@ -9,18 +9,22 @@ const apiKey = 'rrok6oy5kysthf2v2fyh4qja'
 formEl.addEventListener('submit', e => {
   e.preventDefault()
 
-  const script = document.createElement('script')
   const query = queryEl.value
-
   if (!query) return
 
   routie(`listings/${query}`)
+  fetchResults(query)
+});
+
+function fetchResults(query) {
+  const script = document.createElement('script')
 
   script.setAttribute('src', `https://openapi.etsy.com/v2/listings/active.js?callback=EtsySearch.callback&api_key=${apiKey}&limit=5&keywords=${query}&fields=title,url,price`)
-
   document.head.appendChild(script)
   document.head.removeChild(script)
-});
+}
+
+routie('listings/:query', fetchResults);
 
 export function callback(data) {
   data.results.forEach(result => {
